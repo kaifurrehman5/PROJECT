36671049fea3307a6e1afa4f0e73ec4990cdb2e2
kaifurrehman5 <innocentkaif11@gmail.com>
2025-02-03 21:32:00 +0500

    new file

---
+++ HOME/home.js
@@ -0,0 +1,145 @@
+     var st = document.getElementsByTagName("section")[1]
+     st.style.display="none"
+
+     var st = document.getElementsByTagName("section")[2]
+     st.style.display="none"
+
+     var st = document.getElementsByTagName("section")[3]
+     st.style.display="none"
+
+     var st = document.getElementsByTagName("section")[4]
+     st.style.display="none"
+
+     var st = document.getElementsByTagName("section")[5]
+     st.style.display="none"
+
+     var st = document.getElementsByTagName("section")[6]
+     st.style.display="none"
+
+
+
+     document.getElementById("graphic").addEventListener('click',function(){
+
+        var st = document.getElementsByTagName("section")[1]
+        st.style.display="block"
+   
+        var st = document.getElementsByTagName("section")[2]
+        st.style.display="none"
+   
+        var st = document.getElementsByTagName("section")[3]
+        st.style.display="none"
+   
+        var st = document.getElementsByTagName("section")[4]
+        st.style.display="none"
+   
+        var st = document.getElementsByTagName("section")[5]
+        st.style.display="none"
+   
+        var st = document.getElementsByTagName("section")[6]
+        st.style.display="none"
+     })
+
+     document.getElementById("video").addEventListener('click',function(){
+
+        var st = document.getElementsByTagName("section")[1]
+        st.style.display="none"
+   
+        var st = document.getElementsByTagName("section")[2]
+        st.style.display="block"
+   
+        var st = document.getElementsByTagName("section")[3]
+        st.style.display="none"
+   
+        var st = document.getElementsByTagName("section")[4]
+        st.style.display="none"
+   
+        var st = document.getElementsByTagName("section")[5]
+        st.style.display="none"
+   
+        var st = document.getElementsByTagName("section")[6]
+        st.style.display="none"
+     })
+
+     document.getElementById("writter").addEventListener('click',function(){
+
+        var st = document.getElementsByTagName("section")[1]
+        st.style.display="none"
+   
+        var st = document.getElementsByTagName("section")[2]
+        st.style.display="none"
+   
+        var st = document.getElementsByTagName("section")[3]
+        st.style.display="block"
+   
+        var st = document.getElementsByTagName("section")[4]
+        st.style.display="none"
+   
+        var st = document.getElementsByTagName("section")[5]
+        st.style.display="none"
+   
+        var st = document.getElementsByTagName("section")[6]
+        st.style.display="none"
+     })
+
+     document.getElementById("data").addEventListener('click',function(){
+
+        var st = document.getElementsByTagName("section")[1]
+        st.style.display="none"
+   
+        var st = document.getElementsByTagName("section")[2]
+        st.style.display="none"
+   
+        var st = document.getElementsByTagName("section")[3]
+        st.style.display="none"
+   
+        var st = document.getElementsByTagName("section")[4]
+        st.style.display="block"
+   
+        var st = document.getElementsByTagName("section")[5]
+        st.style.display="none"
+   
+        var st = document.getElementsByTagName("section")[6]
+        st.style.display="none"
+     })
+
+     document.getElementById("website").addEventListener('click',function(){
+
+        var st = document.getElementsByTagName("section")[1]
+        st.style.display="none"
+   
+        var st = document.getElementsByTagName("section")[2]
+        st.style.display="none"
+   
+        var st = document.getElementsByTagName("section")[3]
+        st.style.display="none"
+   
+        var st = document.getElementsByTagName("section")[4]
+        st.style.display="none"
+   
+        var st = document.getElementsByTagName("section")[5]
+        st.style.display="block"
+   
+        var st = document.getElementsByTagName("section")[6]
+        st.style.display="none"
+     })
+
+     document.getElementById("software").addEventListener('click',function(){
+
+        var st = document.getElementsByTagName("section")[1]
+        st.style.display="none"
+   
+        var st = document.getElementsByTagName("section")[2]
+        st.style.display="none"
+   
+        var st = document.getElementsByTagName("section")[3]
+        st.style.display="none"
+   
+        var st = document.getElementsByTagName("section")[4]
+        st.style.display="none"
+   
+        var st = document.getElementsByTagName("section")[5]
+        st.style.display="none"
+   
+        var st = document.getElementsByTagName("section")[6]
+        st.style.display="block"
+     })
